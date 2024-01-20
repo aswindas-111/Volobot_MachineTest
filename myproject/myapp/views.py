e3d135367788dcd8c49683f9a4f74b17f5145e81
ASWINDAS <cvaswin1997@gmail.com>
2024-01-20 19:18:24 +0530

    Add docstring for code readability

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,17 +3,21 @@ from .models import *
 from .serializers import SectionSerializer, StudentSerializer
 
 class SectionListCreateView(generics.ListCreateAPIView):
+    '''API endpoint that allows the listing and creation of sections'''
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
 class SectionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    '''API endpoint that allows the retrieval, updating, and deletion of a specific section'''
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
 class StudentListCreateView(generics.ListCreateAPIView):
+    '''API endpoint that allows the listing and creation of students'''
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    '''API endpoint that allows the retrieval, updating, and deletion of a specific student'''
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
